ee99fd8bbfa689b5d97e376083706e3f38b73922
ririthevent <ririthevent@gmail.com>
2015-03-12 18:06:21 +0900

    실습
    실습

--- todos.js
+++ todos.js
@@ -1,3 +1,32 @@
 //start...
 
-console.log('hello world');
+//console.log('hello world');
+
+
+function getDom(id){
+  return document.getElementById(id);
+}
+
+//var todoStringField = document.getElementById('todoString');
+var todoStringField = getDom('todoString');
+var  form = document.getElementById('todoForm');
+
+form.addEventListener('keyup', function(){
+      //console.log(event.keyCode);
+      //console.log( todoStringField, todoStringField.value);
+      if(event.keyCode === 13){
+        console.log(todoStringField.value);
+        var newTodo = todoStringField.value;
+        todoStringField.value = "";
+
+        var listDom = getDom('todoList');
+        listDom.innerHTML += '\
+            <li>\
+                <button class="delete">×</button>\
+                <input type="checkbox" class="toggle-checked">\
+                <span class="text"> ' + newTodo +'</span>\
+            </li>'
+      }
+});
+
+//console.log( todoStringField, todoStringField.value );
